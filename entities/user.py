03da5309fcb702826fefd27eb183ae8edbeadedd
Alumno CITEV <alumnog@itson.edu.mx>
2026-03-20 10:01:55 -0700

    Segundo commit

--- entities/user.py
+++ entities/user.py
@@ -1,7 +1,9 @@
 from persistence.db import get_connection
 from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
-class User:
+from flask_login import UserMixin
+
+class User (UserMixin):
     def __init__(self, id: int, name:str, email:str, password:str):
         self.id = id
         self.name = name
@@ -63,7 +65,7 @@ class User:
             connection = get_connection()
             cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-            sql = "SELECT * FROM 'user' WHERE email=%s"
+            sql = "SELECT id, name, email, password FROM user WHERE email = %s"
             cursor.execute(sql, (email,))
             
             user =cursor.fetchone()
@@ -79,8 +81,33 @@ class User:
                     ""
                 )
             
-
             return None
-        except Exception as ex:
+         except Exception as ex:
+            print(f"Error saving user:{ex}")
+            return False
+         
+    def get_by_id(id):
+         try:
+            connection = get_connection()
+            cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+            sql = "SELECT id, name, email, password FROM user WHERE id = %s"
+            cursor.execute(sql, (id,))
+            
+            user =cursor.fetchone()
+
+            cursor.close()
+            connection.close()
+
+            if user:
+                return User(
+                    user["id"],
+                    user["name"],
+                    user["email"],
+                    user["password"]
+                )
+            
+            return None
+         except Exception as ex:
             print(f"Error saving user:{ex}")
             return False
--- static/js/dogo/signin.js
+++ static/js/dogo/signin.js
@@ -4,18 +4,30 @@ function login(){
     const email = document.getElementById("user-email").value;
     const password = document.getElementById("user-password").value;
 
-    if(email == ""){
-        alert("Debe ingresar su corrreo electronico")
+    if(email === ""){
+        Swal.fire({
+            title: 'Correo electrónico no ingresado',
+            text: 'Debe ingresar su correo electrónico.',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar'
+        });
+        return;
     }
 
-    if(password == ""){
-        alert("Debe ingresar su contraseña")
+    if(password === ""){
+        Swal.fire({
+            title: 'Contraseña no ingresada',
+            text: 'Debe ingresar una contraseña.',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar'
+        });
+        return;
     }
+    
 
     const data = {
-        name: document.getElementById("user-name").value,
-        email: document.getElementById("user-email").value,
-        password: document.getElementById("user-password").value
+        email: email,
+        password: password
 
     }
 
@@ -28,10 +40,16 @@ function login(){
         if(result.success){
             window.location.href = "/welcome";
         }else{
-            alert("Sus datos de acceso no son correctos")
+            Swal.fire({
+            title: 'Datos incorrectos',
+            text: 'Sus datos de acceso no son correctos',
+            icon:  'error',
+            confirmButtonText: 'Aceptar'
+            });
         }
     })
     .catch(error => {
         console.error(error);
     })
+    
 }
